02dc4c4a0499ffc4dfe023feb9d165e92a9e7794
rajaLucy <rajakiranyadavalli@gmail.com>
2026-07-14 16:02:36 +0530

    Reveret updated

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -6106,7 +6106,7 @@ void DisplaySettings::sendMsgThread()
 				}
 				else
 				{
-					LOGWARN("The failure: %s is not connected!",strVideoPort.c_str());
+					LOGWARN("failure: %s is not connected!",strVideoPort.c_str());
 				}
 			}
 			catch (const device::Exception& err)
