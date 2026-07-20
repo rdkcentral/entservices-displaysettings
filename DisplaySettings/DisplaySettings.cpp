1bfd5ca97bd64f26dd6516d46e9801b7693b273a
rajaLucy <rajakiranyadavalli@gmail.com>
2026-07-20 17:56:59 +0530

    updated

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -6106,6 +6106,7 @@ void DisplaySettings::sendMsgThread()
 				}
 				else
 				{
+                    LOGWARN("Simple Log Added");
 					LOGWARN("failure: %s is not connected!",strVideoPort.c_str());
 				}
 			}
