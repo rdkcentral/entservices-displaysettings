1efe91c7bad979a17074c71933b4da4661c9926d
rajaLucy <rajakiranyadavalli@gmail.com>
2026-07-20 18:02:24 +0530

    Reverted

--- plugin/DisplaySettings.cpp
+++ plugin/DisplaySettings.cpp
@@ -6315,7 +6315,6 @@ void DisplaySettings::sendMsgThread()
 				}
 				else
 				{
-                    LOGWARN("Simple Log Added");
 					LOGWARN("failure: %s is not connected!",strVideoPort.c_str());
                     
 				}
