575d6171c99bbb89a3d7c609f3ea16f6a3491e47
rajaLucy <rajakiranyadavalli@gmail.com>
2026-07-20 12:10:39 +0530

    RDKEMW-21092: Add FIX prefix to log message in Request

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -6116,7 +6116,7 @@ void DisplaySettings::sendMsgThread()
 		}
 		if( 0 == (int)connectedDisplays.size())
 		{
-			LOGWARN("No display connected to device (or)device's powerstate is not ON");
+			LOGWARN("[FIX]No display connected to device (or)device's powerstate is not ON");
             return Core::ERROR_GENERAL;
 		}
         return Core::ERROR_NONE;
