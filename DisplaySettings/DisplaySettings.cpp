f1b9f8fbbc3d22c2b19fc8e30e775fbed2bcd4eb
rajaLucy <rajakiranyadavalli@gmail.com>
2026-07-30 11:14:05 +0530

    RDKEMW-21092:Reverted [backported:4d93662]

--- DisplaySettings/DisplaySettings.cpp
+++ DisplaySettings/DisplaySettings.cpp
@@ -6029,7 +6029,7 @@ void DisplaySettings::sendMsgThread()
             switch (format)
             {
                 case dsHDRSTANDARD_SDR:
-                    LOGINFO("Current Video Format: SDR\n");
+                    LOGINFO("Video Format: SDR\n");
                     strValue = "SDR";
                     break;
                 case dsHDRSTANDARD_HDR10:
