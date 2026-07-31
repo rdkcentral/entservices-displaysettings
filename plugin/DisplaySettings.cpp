8225c195de2cb28f596ccf20c51f69e98a8194b0
rajaLucy <rajakiranyadavalli@gmail.com>
2026-07-31 16:36:04 +0530

    Change log added

--- plugin/DisplaySettings.cpp
+++ plugin/DisplaySettings.cpp
@@ -6238,7 +6238,7 @@ void DisplaySettings::sendMsgThread()
             switch (format)
             {
                 case dsHDRSTANDARD_SDR:
-                    LOGINFO("Current is Video Format: SDR\n");
+                    LOGINFO("Video Format: SDR\n");
                     strValue = "SDR";
                     break;
                 case dsHDRSTANDARD_HDR10:
