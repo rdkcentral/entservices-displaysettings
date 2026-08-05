301665186afe88e3ee06044b3a8f8b9e514a5bb7
rajaLucy <rajakiranyadavalli@gmail.com>
2026-08-05 10:55:40 +0530

    RDKEMW-21092: Adding Changes

--- plugin/DisplaySettings.cpp
+++ plugin/DisplaySettings.cpp
@@ -6277,7 +6277,7 @@ void DisplaySettings::sendMsgThread()
 				}
 				else
 				{
-					LOGWARN("failure: %s is not connected!",strVideoPort.c_str());
+					LOGWARN("The failure: %s is not connected!",strVideoPort.c_str());
 				}
 			}
 			catch (const device::Exception& err)
@@ -6287,7 +6287,7 @@ void DisplaySettings::sendMsgThread()
 		}
 		if( 0 == (int)connectedDisplays.size())
 		{
-			LOGWARN("Request() No display connected to the device (or) device's powerstate is not ON");
+			LOGWARN("No display connected to the device (or) device's powerstate is not ON");
             return Core::ERROR_GENERAL;
 		}
         return Core::ERROR_NONE;
