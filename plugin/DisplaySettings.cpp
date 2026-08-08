96442ca11bd05bf3c53ea68b8bfc14a836498daa
rajaLucy <rajakiranyadavalli@gmail.com>
2026-08-08 10:41:50 +0530

    RDKEME-21092:Updated script

--- plugin/DisplaySettings.cpp
+++ plugin/DisplaySettings.cpp
@@ -6277,7 +6277,7 @@ void DisplaySettings::sendMsgThread()
 				}
 				else
 				{
-                    	        LOGWARN("The failure: %s is not the connected!",strVideoPort.c_str());
+                    	        LOGWARN("[error]failure: %s is not the connected!",strVideoPort.c_str());
 
 				}
 			}
