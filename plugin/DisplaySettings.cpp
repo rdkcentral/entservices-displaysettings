dd019f68543dd9a8a38a34754b9335212a0f7587
rajaLucy <rajakiranyadavalli@gmail.com>
2026-08-12 14:35:06 +0530

    RDKEMW-21092:Updated for No conflicts

--- plugin/DisplaySettings.cpp
+++ plugin/DisplaySettings.cpp
@@ -6288,7 +6288,7 @@ void DisplaySettings::sendMsgThread()
 		}
 		if( 0 == (int)connectedDisplays.size())
 		{
-			LOGWARN("No display connected to the device (or) device's powerstate is not ON");
+			LOGWARN("No display connected to device (or) device's powerstate is not ON");
             return Core::ERROR_GENERAL;
 		}
         return Core::ERROR_NONE;
