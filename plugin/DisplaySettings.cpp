e476ef498c89f5f4a77b3fb0156875a8d8600ddb
rajaLucy <rajakiranyadavalli@gmail.com>
2026-08-18 19:50:48 +0530

    RDKEMW-21092:Updated in function

--- plugin/DisplaySettings.cpp
+++ plugin/DisplaySettings.cpp
@@ -6266,7 +6266,7 @@ void DisplaySettings::sendMsgThread()
 				//device::VideoOutputPort vPort = device::Host::getInstance().getVideoOutputPort(strVideoPort.c_str());
 				if (isDisplayConnected(strVideoPort))
 				{
-					bool enable = (newState == "GAME") ? true : false;
+					bool enable = (newState == "GAME" || newState == "game") ? true : false;
 					if(enable){ // Game mode
 					    vPort.getDisplay().setAVIContentType(dsAVICONTENT_TYPE_GAME);
 					    vPort.getDisplay().setAVIScanInformation(dsAVI_SCAN_TYPE_UNDERSCAN);
